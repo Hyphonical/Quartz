7687283fada7184c4228c76ffde8e32e8ecbe730
hyphonic <hyphonical@gmail.com>
2025-08-17 22:01:10 +0200

    fix: update baseUrl in quartz configuration to point to wiki domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.hypho.dev",
+    baseUrl: "wiki.hypho.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
