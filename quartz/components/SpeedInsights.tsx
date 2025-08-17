19805bb3d10bc547ed35c9d32d945f6d715eb67f
hyphonic <hyphonical@gmail.com>
2025-08-17 22:58:24 +0200

    feat: add Vercel Speed Insights integration with prebuild script and update SpeedInsights component

--- quartz/components/SpeedInsights.tsx
+++ quartz/components/SpeedInsights.tsx
@@ -2,17 +2,7 @@ import { QuartzComponent, QuartzComponentProps } from "./types"
 
 export default (() => {
   function SpeedInsights(_: QuartzComponentProps) {
-    return (
-      <script
-        type="module"
-        dangerouslySetInnerHTML={{
-          __html: `
-import { injectSpeedInsights } from "https://esm.sh/@vercel/speed-insights";
-injectSpeedInsights({ sampleRate: 1 });
-`,
-        }}
-      ></script>
-    )
+    return <script type="module" src="/js/speed-insights.js" defer></script>
   }
   return SpeedInsights
 }) satisfies QuartzComponent
