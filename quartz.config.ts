57447f1e0534eaa5f749a1430d257f32a47a6727
inventory69 <admin@sad.run>
2025-09-25 22:34:48 +0200

    fix: update baseUrl in Quartz configuration

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "wiki.voidtales.hypho.dev",
+    baseUrl: "wiki.voidtales.win",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
